f819b9d217df24fd37b1b11a5b8b27a223da7224
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-05 18:21:02 +0300

    add 004 Condition If Else

---
+++ example004_ConditionIfElif/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите своё имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура это Masha");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
+
