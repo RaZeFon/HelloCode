e7a74dad0bca3a389c52ee8feb1de94ff25bb9a7
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-06 11:53:50 +0300

    [задача 1] решение

---
+++ dz_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//первая задача
+Console.Clear();
+Console.Write("Введите первое число ");
+int num_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int num_2 = int.Parse(Console.ReadLine());
+if (num_1 > num_2)
+{
+    Console.Write("max = ");
+    Console.WriteLine(num_1);
+    Console.Write("min = ");
+    Console.WriteLine(num_2);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(num_2);
+    Console.Write("min = ");
+    Console.WriteLine(num_1);
+}
