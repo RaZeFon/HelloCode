54ba4bbc89db1cd5d394c962a8116d68580a598d
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-06 07:44:40 +0300

    [#000] Task

---
+++ Task0/Program.cs
@@ -0,0 +1,13 @@
+﻿//Решение задачи построино на линейных алгоритмах работы программ.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа = {sqr}");
+
+
+//Второе решение основываеться на использовании стандартных математических библиотек.
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа = {sqr1}");
+
