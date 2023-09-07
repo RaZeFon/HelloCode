b986906e33c39a6de310e7b7ee5c421efdb02575
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-07 13:30:16 +0300

    наибольшая цифра цисла

---
+++ Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿int num = new Random().Next(10, 99);
+Console.WriteLine(num);
+int num2 = num / 10;
+int num1 = num % 10;
+if (num1 > num2)
+{
+    Console.Write("макс число ");
+    Console.WriteLine(num1);
+}
+else
+{
+    Console.Write("Макс число ");
+    Console.WriteLine(num2);
+}
