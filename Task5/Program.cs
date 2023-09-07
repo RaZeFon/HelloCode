768ed617f18e911f9eca007e5e1a00e01c6b0d76
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-07 14:57:35 +0300

    наибольшая цифра цисла препод

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,14 +1,31 @@
-﻿int num = new Random().Next(10, 99);
-Console.WriteLine(num);
-int num2 = num / 10;
-int num1 = num % 10;
-if (num1 > num2)
+﻿//int num = new Random().Next(10, 100);
+//Console.WriteLine(num);
+//int num2 = num / 10;
+//int num1 = num % 10;
+//if (num1 > num2)
+//{
+//    Console.Write("макс число ");
+//    Console.WriteLine(num1);
+//}
+//else
+//{
+//    Console.Write("Макс число ");
+//    Console.WriteLine(num2);
+//}
+
+using System;
+using static System.Console;
+
+Clear();
+int number = new Random().Next(10, 100);
+int a1 = number / 10;
+int a2 = number % 10;
+
+if (a1 > a2)
 {
-    Console.Write("макс число ");
-    Console.WriteLine(num1);
+    WriteLine($"{number}->{a1}");
 }
 else
 {
-    Console.Write("Макс число ");
-    Console.WriteLine(num2);
+    WriteLine($"{number}->{a2}"); 
 }
