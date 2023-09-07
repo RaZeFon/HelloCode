f2b7c7d273198a8839e9d07c1e4186ac66602534
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-07 20:05:55 +0300

    кратное 7 и 23 препод

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,21 +1,38 @@
-﻿using System;
+﻿//using System;
+//using static System.Console;
+
+//Clear();
+//Write("Введите число: ");
+//int number = int.Parse(ReadLine());
+//if (number % 7 == 0)
+//{
+//    if (number % 23 == 0)
+//    {
+//        WriteLine("Да");
+//    }
+//    if (number % 23 != 0)
+//    {
+//        WriteLine("Нет");
+//    }
+//}
+//else
+//{
+//    WriteLine("Нет");
+//}
+
+
+using System;
 using static System.Console;
 
 Clear();
 Write("Введите число: ");
 int number = int.Parse(ReadLine());
-if (number % 7 == 0)
+if ((number % 7 == 0)&&(number % 23 == 0))
 {
-    if (number % 23 == 0)
-    {
-        WriteLine("Да");
-    }
-    if (number % 23 != 0)
-    {
-        WriteLine("Нет");
-    }
+    WriteLine($"{number}->Да");
 }
 else
 {
-    WriteLine("Нет");
+    WriteLine($"{number}->Нет");
 }
+
