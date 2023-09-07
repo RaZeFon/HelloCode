ec0baba1ce0f9706732bec5250d4404fbaf22886
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-07 18:23:26 +0300

    кратность препод

--- Task7/Program.cs
+++ Task7/Program.cs
@@ -1,19 +1,36 @@
 ﻿
+//using System;
+//using static System.Console;
+//Clear();
+//Write("Первое число: ");
+//int number1 = Int32.Parse(ReadLine());
+//Write("Второе число: ");
+//int number2 = Int32.Parse(ReadLine());
+//int num = number1 / number2;
+//int sum = number2 * num;
+//if (number1 != sum)
+//{
+//    int ost = number1 - sum;
+//    WriteLine($"{number2} Некратно {number1}, остаток {ost}");
+//}
+//else
+//{
+//    WriteLine($"{number2} кратно {number1}");
+//}
+
+
 using System;
 using static System.Console;
 Clear();
-Write("Первое число: ");
-int number1 = Int32.Parse(ReadLine());
-Write("Второе число: ");
-int number2 = Int32.Parse(ReadLine());
-int num = number1 / number2;
-int sum = number2 * num;
-if (number1 != sum)
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine());
+if(a % b == 0)
 {
-    int ost = number1 - sum;
-    WriteLine($"{number2} Некратно {number1}, остаток {ost}");
+    WriteLine("кратно");
 }
 else
 {
-    WriteLine($"{number2} кратно {number1}");
+    WriteLine($"не кратно {a % b}");
 }
