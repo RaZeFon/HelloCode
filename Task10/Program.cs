3cda6e40f6615a1f75da92bbaa1a96325e4c9564
Evgenii Golichenkov <neonmaster22@yandex.ru>
2023-09-08 08:26:24 +0300

    задачи на координаты

---
+++ Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите координату X: ");
+int x = int.Parse(ReadLine());
+Write("Введите координату Y: ");
+int y = int.Parse(ReadLine());
+
+if ( x > 0 && y > 0)
+{
+    WriteLine("1 координатная четверть");
+}
+if ( x < 0 && y > 0)
+{
+    WriteLine("2 координатная четверть");
+}
+if ( x < 0 && y < 0)
+{
+    WriteLine("3 координатная четверть");
+}
+if ( x > 0 && y < 0)
+{
+    WriteLine("4 координатная четверть");
+}
+
+
+//             ^ y
+//             |
+//      2      |       1
+//             |
+//---------------------------->
+//             |             x
+//      3      |       4
+//             |
+//             |  
---
+++ Task11/Program.cs
@@ -0,0 +1,73 @@
+﻿//using System;
+//using static System.Console;
+
+//Clear();
+//Write("Введите координатную четверть: ");
+//int quarter = int.Parse(ReadLine());
+//if (quarter == 1)
+//{
+//    WriteLine("x = 1 до бесконечности");
+//    WriteLine("y = 1 до бесконечности");
+//}
+//if (quarter == 2)
+//{
+//    WriteLine("x = -1 до минус бесконечности");
+//    WriteLine("y = 1 до бесконечности");
+//}
+//if (quarter == 3)
+//{
+//    WriteLine("x = -1 до минус бесконечности");
+//    WriteLine("y = -1 до минус бесконечности");
+//}
+//if (quarter == 4)
+//{
+//    WriteLine("x = 1 до бесконечности");
+//    WriteLine("y = -1 до минус бесконечности");
+//}
+
+
+//             ^ y
+//             |
+//      2      |       1
+//             |
+//---------------------------->
+//             |             x
+//      3      |       4
+//             |
+//             |  
+
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите номер четверти: ");
+int n = int.Parse(ReadLine());
+switch(n)
+{
+    case 1:
+    {
+        WriteLine("x > 0,  y > 0");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("x < 0,  y > 0");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("x < 0,  y < 0");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("x > 0,  y < 0");
+        break;
+    }
+    default:
+    {
+        WriteLine("Неверная четверть");
+        break;
+    }
+}
