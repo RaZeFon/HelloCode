40626710c8a5d8cee63747f75bf36a71b1019f4b
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-12 09:04:57 +0300

    перед мержем

---
+++ dz03/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+num_A
+num_B
+*/
+
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine($"Натуральная степень = {Degree(A, B)}");
+int Degree(int num_A, int num_B)
+{
+    int result = 0;
+    for (int i = 1; num_B > i; i++)
+    {
+        result = num_A * num_B;
+        i++;
+    }
+    return result;
+}
