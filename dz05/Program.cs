00e054e95178cb51bf4a9d58cfd28fbcaca845e7
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-15 18:43:36 +0300

    dz 5

---
+++ dz05/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите числа через пробел: ");
+int[] array = GetArrayFromString(Readline());
+
+
+int[] GetArrayFromString(string strArray)
+{
+    string StrNums = strArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[StrNums.Length];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] Convert.ToInt32(StrNums[i]);
+    }
+    return result;
+}
+
+int[] AboveZero(int[] inArray)
+{
+    int count = 0;
+     
+}
