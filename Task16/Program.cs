acd56c50dc74db1d4bb0c68b3f3c202e8f0bb517
Evgeny Golichenkov <neonmaster22@yandex.ru>
2023-09-17 08:31:59 +0300

    task 16

---
+++ Task16/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите размер массива через пробел: ");
+string[] nums = ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[,] array = GetArray(int.Parse(nums[0]), int.Parse(nums[1]), -10, 10);
+PrintArray(array);
+
+int[,] GetArray(int rows, int columns, int min, int max)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(min, max +1);
+        }
+    }
+    return result;
+}
+
+// К размерности массива можно обращаться через метод GetLength
+// метод возвращает максиммальное значение или количествоэлементов
+// которое указали в качестве размера.
+
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();        
+    }
+}
+*/
+
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите количество строк: ");
+int m = int.Parse(ReadLine());
+Write("Ведите количество столбцов: ");
+int n = int.Parse(ReadLine());
+int[,] array = GetArray(m, n);
+PrintArray(array);
+
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = i + j;
+        }
+    }
+    return result;
+}
+ 
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Write($"{inArray[i, j]} ");
+        }
+        WriteLine();        
+    }
+}
